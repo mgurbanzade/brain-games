1318dcdc054d23e6e470c5c9e2caba54f5e37209
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2018-06-19 09:33:08 +0400

    Fix eslint and update version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+const readlineSync = require('readline-sync');
+
 const askPlayerName = () => {
-  const readlineSync = require('readline-sync');
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
 };
